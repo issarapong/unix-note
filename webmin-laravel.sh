67128a15b76c9e625f59a6ccb4f1b303b1d108f7
GNOPARASSI <mr.issarapong@gmail.com>
2021-01-28 23:44:41 +0700

    Update webmin-laravel.sh

--- webmin-laravel.sh
+++ webmin-laravel.sh
@@ -4,7 +4,7 @@ apt-get update
 echo “deb http://download.webmin.com/download/repository sarge contrib”  >> /etc/apt/sources.list
 wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
 apt-get update
-apt-get -y install webmin
+apt-get install webmin
 apt install apache2
 apt-get install php php-cli php-fpm php-json php-common php-mysql php-zip unzip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
 wget -O composer-setup.php https://getcomposer.org/installer
